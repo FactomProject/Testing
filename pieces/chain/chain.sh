56bddc43a1f11d8d134d7da523485d67ba2b41e7
Robert Baker <drobertbaker@factom.org>
2015-07-16 19:21:15 -0500

    removed folder

--- pieces/chain/chain.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "3	MAKE A CHAIN"
-echo "3.1	MAKE A CHAIN"
-factom-cli mkchain -e level1 -e level2 <~/testing/test-plans-and-scripts/chain/1st-entry
--- pieces/factoidtx/factoidtx.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-factom-cli -w ~/testing/ecwallet balance ec
-factom-cli -w ~/testing/ecwallet balance factoid
-
