08350335bf5128e7891f59774be6868c8518aa09
Robert Baker <drobertbaker@factom.org>
2015-07-31 13:12:06 -0500

    script updates

--- Robert-API/full/full.sh
+++ Robert-API/full/full.sh
@@ -1,5 +1,7 @@
 #!/bin/bash +x
 
+echo WAITING 60 SECONDS FOR FACTOMD TO INITIALIZE
+sleep 60
 echo
 echo 1 WALLETS
 echo
@@ -7,12 +9,43 @@ echo 1.1 FACTOID WALLET
 echo
 echo 1.1.1 CREATE FACTOID WALLET ADDRESSES        
 echo
-echo 1.1.1.1 CREATE LEGITIMATE FACTOID WALLET ADDRESS NAMES        
+echo 1.1.1.1 CREATE LEGITIMATE FACTOID WALLET ADDRESSES CONTAINING 2000 FACTOIDS        
+factom-cli generateaddress fct dummy0
+factom-cli generateaddress fct dummy1
+factom-cli generateaddress fct dummy2
+factom-cli generateaddress fct dummy3
+factom-cli generateaddress fct dummy4
+factom-cli generateaddress fct dummy5
+factom-cli generateaddress fct dummy6
+factom-cli generateaddress fct dummy7
+factom-cli generateaddress fct dummy8
+factom-cli generateaddress fct dummy9
 FACTOIDWALLETADDRESSKEY01=$(factom-cli generateaddress fct factoid-wallet-address-name01 | awk '{print $3}')
 echo factoid-wallet-address-name01 = $FACTOIDWALLETADDRESSKEY01
+factom-cli newtransaction dummytransaction1
+factom-cli addinput dummytransaction1 dummy1 2001
+factom-cli addoutput dummytransaction1 factoid-wallet-address-name01 2000
+factom-cli sign dummytransaction1
+factom-cli submit dummytransaction1
+factom-cli balance fct factoid-wallet-address-name01
 echo 
 FACTOIDWALLETADDRESSKEY02=$(factom-cli generateaddress fct factoid-wallet-address-name02 | awk '{print $3}')
 echo factoid-wallet-address-name02 = $FACTOIDWALLETADDRESSKEY02
+factom-cli newtransaction dummytransaction2
+factom-cli addinput dummytransaction2 dummy2 2001
+factom-cli addoutput dummytransaction2 factoid-wallet-address-name02 2000
+factom-cli sign dummytransaction2
+factom-cli submit dummytransaction2
+factom-cli balance fct factoid-wallet-address-name02
+echo 
+FACTOIDWALLETADDRESSKEY03=$(factom-cli generateaddress fct factoid-wallet-address-name03 | awk '{print $3}')
+echo factoid-wallet-address-name03 = $FACTOIDWALLETADDRESSKEY03
+factom-cli newtransaction dummytransaction3
+factom-cli addinput dummytransaction3 dummy3 2001
+factom-cli addoutput dummytransaction3 factoid-wallet-address-name03 2000
+factom-cli sign dummytransaction3
+factom-cli submit dummytransaction3
+factom-cli balance fct factoid-wallet-address-name03
 echo 
 echo 1.1.1.2 TRY TO CREATE TOO LONG FACTOID WALLET ADDRESS NAME       
 factom-cli generateaddress fct factoid-wallet-address-name890123
@@ -26,7 +59,7 @@ echo
 echo 1.1.1.5 TRY TO CREATE FACTOID WALLET ADDRESS NAME CONTAINING '"~"'        
 factom-cli generateaddress fct "'F~02'"
 echo 
-echo 1.1.1.6 TRY TO CREATE FACTOID WALLET ADDRESS NAME ENDING WITH '"~"'        
+echo 1.1.1.6 TRY TO CREATE FACTOID WALLET ADDRESS NAME ENDING WITH '"!"'        
 factom-cli generateaddress fct "'F02!'"
 echo 
 echo 1.1.1.7 TRY TO CREATE FACTOID WALLET ADDRESS NAME STARTING WITH "@"        
@@ -106,6 +139,8 @@ factom-cli balance fct factoid-wallet-address-name01
 factom-cli balance fct $FACTOIDWALLETADDRESSKEY01
 factom-cli balance fct factoid-wallet-address-name02
 factom-cli balance fct $FACTOIDWALLETADDRESSKEY02
+factom-cli balance fct factoid-wallet-address-name03
+factom-cli balance fct $FACTOIDWALLETADDRESSKEY03
 echo 
 echo 1.1.2.3 REQUEST BALANCE OF NON-EXISTENT FACTOID WALLET ADDRESS
 factom-cli balance fct factom-cli balance fct xxx
@@ -153,34 +188,40 @@ echo 1.2.2.4 REQUEST BALANCE OF ENTRY CREDIT WALLET ADDRESS NAME TOO LONG
 factom-cli balance ec hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhaaaa
 echo
 
-echo 2  TRANSACTIONS        
+echo 2 TRANSACTIONS        
 echo
-echo 2.1  TRANSACTION FEE        
+echo 2.1 TRANSACTION FEE        
 factom-cli getfee
 echo
 
 echo 2.2 CREATE TRANSACTION WITH NO INPUTS OUTPUTS OR ENTRY CREDITS
 echo
-echo 2.2.1  CREATE TRANSACTION        
+echo 2.2.1 CREATE TRANSACTION        
 factom-cli newtransaction transaction01
 echo
-echo 2.2.2  SUBMIT TRANSACTION        
+echo 2.2.2 SUBMIT TRANSACTION        
 factom-cli submit transaction01
 echo
-echo 2.2.3  SIGN TRANSACTION        
+echo 2.2.3 SIGN TRANSACTION        
 factom-cli sign transaction01
 echo
-echo 2.2.4  ADD JUST ENOUGH INPUT TO COVER TRANSACTION FEE        
+echo 2.2.4 ADD JUST ENOUGH INPUT TO COVER TRANSACTION FEE        
 factom-cli addinput transaction01 factoid-wallet-address-name01 1
 echo
-echo 2.2.5  RE-SIGN TRANSACTION        
+echo 2.2.5 RE-SIGN TRANSACTION        
 factom-cli sign transaction01
 echo
-echo 2.2.6  SUBMIT TRANSACTION        
+echo 2.2.6 TRY TO RECREATE TRANSACTION THAT HASN\'T BEEN ACCEPTED        
+factom-cli newtransaction transaction01
+echo
+echo 2.2.7 SUBMIT TRANSACTION        
 factom-cli submit transaction01
 echo
-echo 2.2.7  VERIFY WALLET BALANCES        
-factom-cli getaddresses
+echo 2.2.8 TRY TO RECREATE TRANSACTION THAT HAS BEEN ACCEPTED        
+factom-cli newtransaction transaction01
+echo
+echo 2.2.9 VERIFY WALLET BALANCE        
+factom-cli balance fct factoid-wallet-address-name01
 echo
 
 echo 2.3 CREATE TRANSACTION WITH NO INPUTS
@@ -189,7 +230,7 @@ echo 2.3.1 CREATE TRANSACTION
 factom-cli newtransaction transaction02
 echo
 echo 2.3.2 ADD OUTPUT OF 3 FACTOIDS TO TRANSACTION        
-factom-cli addoutput transaction02 factoid-wallet-address-name02 3
+factom-cli addoutput transaction02 factoid-wallet-address-name01 3
 echo
 echo 2.3.3 SIGN TRANSACTION        
 factom-cli sign transaction02
@@ -197,8 +238,8 @@ echo
 echo 2.3.4 SUBMIT TRANSACTION        
 factom-cli submit transaction02
 echo
-echo 2.3.5 VERIFY WALLET BALANCES        
-factom-cli getaddresses
+echo 2.3.5 VERIFY WALLET BALANCE        
+factom-cli balance fct factoid-wallet-address-name01
 echo
 
 echo 2.4 CREATE TRANSACTION WITH NO OUTPUTS OR ENTRY CREDITS
@@ -218,8 +259,8 @@ echo
 echo 2.4.5 SUBMIT TRANSACTION        
 factom-cli submit transaction03
 echo
-echo 2.4.6 VERIFY WALLET BALANCES        
-factom-cli getaddresses
+echo 2.4.6 VERIFY WALLET BALANCE        
+factom-cli balance fct factoid-wallet-address-name01
 echo
 
 echo 2.5 CREATE TRANSACTION WITH INPUT TO ENTRY CREDITS
@@ -233,8 +274,8 @@ echo
 echo 2.5.3 ADD 6 FACTOIDS OF ENTRY CREDITS FOR 1ST ENTRY CREDIT WALLET TO TRANSACTION        
 factom-cli addecoutput transaction04 ec-wallet-address-name01 6
 echo
-echo 2.5.4 ADD -6 FACTOIDS OF ENTRY CREDITS FOR 1ST ENTRY CREDIT WALLET TO TRANSACTION        
-factom-cli addecoutput transaction04 ec-wallet-address-name01 -6
+echo 2.5.4 ADD -3 FACTOIDS OF ENTRY CREDITS FOR 1ST ENTRY CREDIT WALLET TO TRANSACTION        
+factom-cli addecoutput transaction04 ec-wallet-address-name01 -3
 echo
 echo 2.5.5 SIGN TRANSACTION        
 factom-cli sign transaction04
@@ -243,7 +284,8 @@ echo 2.5.6 SUBMIT TRANSACTION
 factom-cli submit transaction04
 echo
 echo 2.5.7 VERIFY WALLET BALANCES        
-factom-cli getaddresses
+factom-cli balance fct factoid-wallet-address-name01
+factom-cli balance ec ec-wallet-address-name01
 echo
 
 echo 2.6 CREATE TRANSACTION WITH INPUT TO OUTPUT
@@ -266,8 +308,9 @@ echo
 echo 2.6.6 SUBMIT TRANSACTION        
 factom-cli submit transaction05
 echo
-echo 2.6.7 VERIFY WALLET BALANCES        
-factom-cli getaddresses
+echo 2.6.7 VERIFY WALLET BALANCE        
+factom-cli balance fct factoid-wallet-address-name01
+factom-cli balance fct xxx
 echo
 
 echo 2.7 CREATE TRANSACTION WITH INPUT TO OUTPUT AND ENTRY CREDITS        
@@ -297,7 +340,9 @@ echo 2.7.6 SUBMIT TRANSACTION
 factom-cli submit transaction06
 echo
 echo 2.7.7 VERIFY WALLET BALANCES        
-factom-cli getaddresses
+factom-cli balance fct factoid-wallet-address-name01
+factom-cli balance fct factoid-wallet-address-name02
+factom-cli balance ec $ECWALLETADDRESSKEY01
 echo
 
 echo 2.8 CREATE TRANSACTION WITH MULTIPLE INPUT OUTPUT AND ENTRY CREDITS        
@@ -348,7 +393,10 @@ echo 2.8.7 SUBMIT TRANSACTION
 factom-cli submit transaction07
 echo
 echo 2.8.8 VERIFY WALLET BALANCES        
-factom-cli getaddresses
+factom-cli balance fct factoid-wallet-address-name01
+factom-cli balance fct factoid-wallet-address-name02
+factom-cli balance ec $ECWALLETADDRESSKEY01
+factom-cli balance ec $ECWALLETADDRESSKEY02
 echo
 
 echo 2.9 TEST TRANSACTION PARAMETER LIMITS
@@ -363,7 +411,6 @@ factom-cli balance fct factoid-wallet-address-name01
 echo
 
 echo 2.9.2 ADD INPUT OF 0 FACTOIDS TO TRANSACTION        
-factom-cli generateaddress fct factoid-wallet-address-name03
 factom-cli balance fct factoid-wallet-address-name03
 factom-cli newtransaction transaction09
 factom-cli addinput transaction09 factoid-wallet-address-name03 0
@@ -420,16 +467,46 @@ factom-cli submit transaction15
 factom-cli balance fct factoid-wallet-address-name03
 echo
 
-# CHECK ALL CHARACTERS EVENTUALLY (!@+*ETC)
+echo 2.9.9 CREATE LARGEST ALLOWABLE TRANSACTION \(10KB\)        
+factom-cli newtransaction transaction16
+for ((i=0; i < 75; i++)); do
+    factom-cli newtransaction transfertransaction$i
+    factom-cli addinput transfertransaction$i dummy0 2
+    factom-cli generateaddress fct input-address-name$i
+    factom-cli addoutput transfertransaction$i input-address-name$i 1.99
+    factom-cli sign transfertransaction$i
+    factom-cli submit transfertransaction$i
+    j=`echo "scale=8;$i/100" | bc`
+    factom-cli addinput transaction16 input-address-name$i $j
+done
+factom-cli sign transaction16
+factom-cli submit transaction16
+echo
+
+echo 2.9.10 TRY TO CREATE TOO LARGEST ALLOWABLE TRANSACTION \>\(10KB\)        
+factom-cli newtransaction transaction17
+factom-cli newtransaction transfertransaction75
+factom-cli addinput transfertransaction75 dummy0 2
+factom-cli generateaddress fct input-address-name75
+factom-cli addoutput transfertransaction75 input-address-name75 1.99
+factom-cli sign transfertransaction75
+factom-cli submit transfertransaction75
+for ((i=0; i < 76; i++)); do
+    j=`echo "scale=8;$i/100" | bc`
+    factom-cli addinput transaction17 input-address-name$i $j
+done
+factom-cli sign transaction17
+factom-cli submit transaction17
+echo
 
 echo 3 CHAIN
 echo
-echo 3.1 MAKE A CHAIN
+echo 3.1 MAKE CHAINS
 echo
 echo 3.1.1 TRY TO MAKE CHAIN USING NON-EXISTENT ENTRY CREDIT ADDRESS
 echo
 echo Proposed New Chain ID
-CHAINID=$(factom-cli mkchain -e 111111 -e 222222 xxx <~/testing/testing/test-plans-and-scripts/Robert-API/entries/1st-entry | awk '{print $3}')
+CHAINID=$(factom-cli mkchain -e 123456 -e 123456 xxx <~/testing/testing/test-plans-and-scripts/Robert-API/entries/1st-entry | awk '{print $3}')
 echo $CHAINID
 echo
 echo WAITING 60 SECONDS FOR DIRECTORY BLOCK TO CLOSE
@@ -440,11 +517,26 @@ echo $HEAD
 factom-cli balance ec xxx
 echo
 
-echo 3.1.2 MAKE CHAIN USING ENTRY CREDIT ADDRESS NAME
+echo 3.1.2 TRY TO MAKE CHAIN WITH ENTRY SIZE \>10KB
 echo
 factom-cli balance ec ec-wallet-address-name01
 echo Proposed New Chain ID
-CHAINIDGOOD=$(factom-cli mkchain -e 111111 -e 222222 ec-wallet-address-name01 <~/testing/testing/test-plans-and-scripts/Robert-API/entries/1st-entry | awk '{printf $3}')
+CHAINID=$(factom-cli mkchain -e 123456 -e 1234567 ec-wallet-address-name01 <~/testing/testing/test-plans-and-scripts/Robert-API/entries/1st-entry | awk '{printf $3}')
+echo $CHAINID
+echo
+echo WAITING 60 SECONDS FOR DIRECTORY BLOCK TO CLOSE
+sleep 60
+echo Chain Head Merkelroot
+HEAD=$(factom-cli get chain $CHAINID)
+echo $HEAD
+factom-cli balance ec ec-wallet-address-name01
+echo
+
+echo 3.1.3 MAKE CHAIN WITH MAXIMUM ENTRY SIZE \(10240 BYTES\) USING ENTRY CREDIT ADDRESS NAME
+echo
+factom-cli balance ec ec-wallet-address-name01
+echo Proposed New Chain ID
+CHAINIDGOOD=$(factom-cli mkchain -e 123456 -e 123456 ec-wallet-address-name01 <~/testing/testing/test-plans-and-scripts/Robert-API/entries/1st-entry | awk '{printf $3}')
 echo $CHAINIDGOOD
 echo
 echo WAITING 60 SECONDS FOR DIRECTORY BLOCK TO CLOSE
@@ -455,11 +547,11 @@ echo $HEADGOOD
 factom-cli balance ec ec-wallet-address-name01
 echo
 
-echo 3.1.3 MAKE CHAIN THAT ALREADY EXISTS
+echo 3.1.4 MAKE CHAIN THAT ALREADY EXISTS
 echo
 factom-cli balance ec ec-wallet-address-name01
 echo Proposed New Chain ID
-CHAINID=$(factom-cli mkchain -e 111111 -e 222222 ec-wallet-address-name01 <~/testing/testing/test-plans-and-scripts/Robert-API/entries/1st-entry | awk '{printf $3}')
+CHAINID=$(factom-cli mkchain -e 123456 -e 123456 ec-wallet-address-name01 <~/testing/testing/test-plans-and-scripts/Robert-API/entries/1st-entry | awk '{printf $3}')
 echo $CHAINID
 echo
 echo WAITING 60 SECONDS FOR DIRECTORY BLOCK TO CLOSE
@@ -470,10 +562,10 @@ echo $HEAD
 factom-cli balance ec ec-wallet-address-name01
 echo
 
-echo 3.1.4 MAKE SAME CHAIN USING DIFFERENT EXTERNAL ID
+echo 3.1.5 MAKE SAME CHAIN USING DIFFERENT INITIAL ENTRY
 echo
 echo Proposed New Chain ID
-CHAINID=$(factom-cli mkchain -e 111111 -e 222222 ec-wallet-address-name01 <~/testing/testing/test-plans-and-scripts/Robert-API/entries/2nd-entry | awk '{printf $3}')
+CHAINID=$(factom-cli mkchain -e 123456 -e 123456 ec-wallet-address-name01 <~/testing/testing/test-plans-and-scripts/Robert-API/entries/2nd-entry | awk '{printf $3}')
 echo $CHAINID
 echo
 echo WAITING 60 SECONDS FOR DIRECTORY BLOCK TO CLOSE
@@ -484,7 +576,7 @@ echo $HEAD
 factom-cli balance ec $ECWALLETADDRESSKEY01
 echo
 
-echo 3.1.5 MAKE CHAIN USING A ENTRY CREDIT ADDRESS KEY
+echo 3.1.6 MAKE CHAIN USING A ENTRY CREDIT ADDRESS KEY
 echo
 HOUR=$( date +%H )
 MINUTE=$( date +%M )
@@ -500,6 +592,8 @@ echo $HEAD
 factom-cli balance ec $ECWALLETADDRESSKEY01
 echo
 
+echo 3.2 ENTRY BLOCK CHAIN
+echo
 echo 3.2 SHOW ENTRY BLOCK CHAIN
 ENTRYKEYMERKELROOT=$HEADGOOD
 until [ "$ENTRYKEYMERKELROOT" =  '0000000000000000000000000000000000000000000000000000000000000000' -o "$ENTRYKEYMERKELROOT" = "" ]; do
@@ -536,7 +630,7 @@ until [ "$ENTRYKEYMERKELROOT" =  '0000000000000000000000000000000000000000000000
 done
 echo
 
-echo 3.3 ADD ENTRY TO CHAIN
+echo 3.2.2 ADD ENTRY TO CHAIN
 factom-cli put -e 333333 -e 444444 -c $CHAINIDGOOD ec-wallet-address-name01 <~/testing/testing/test-plans-and-scripts/Robert-API/entries/2nd-entry
 echo Chain Head Merkelroot
 echo WAITING 60 SECONDS FOR DIRECTORY BLOCK TO CLOSE
@@ -544,7 +638,7 @@ sleep 60
 HEADGOOD=$(factom-cli get chain $CHAINIDGOOD)
 echo $HEADGOOD
 
-echo 3.4 TRACE EXTENDED ENTRY BLOCK CHAIN
+echo 3.2.3 TRACE ENTRY BLOCK CHAIN
 ENTRYKEYMERKELROOT=$HEADGOOD
 until [ "$ENTRYKEYMERKELROOT" =  '0000000000000000000000000000000000000000000000000000000000000000' -o "$ENTRYKEYMERKELROOT" = "" ]; do
     RESULT=$(factom-cli get eblock $ENTRYKEYMERKELROOT)
@@ -580,8 +674,25 @@ until [ "$ENTRYKEYMERKELROOT" =  '0000000000000000000000000000000000000000000000
 done
 echo
 
+: <<'EOF'
+
+echo 3.2.4 MAKE MANY ENTRIES
+echo
+for ((i=0; i < 100; i++)); do
+    for ((j=0; j < 500; j++)); do
+    echo $i $j | factom-cli put -c $CHAINIDGOOD -e $i -e $j dummy5 &
+    done
+echo $(($i+1)) of 100 groups of 500 finished
+sleep 1
+done
+echo
+
+EOF
+
+echo 3.3 DIRECTORY BLOCK CHAIN
+echo
+echo 3.3.1 TRACE DIRECTORY BLOCK CHAIN
 #'s/[{}]//g'  | sed 's/^.//'  | sed 's/.$//'
-echo 3.5  TRACE DIRECTORY BLOCK CHAIN
 HEAD=$(factom-cli get head)
 echo $HEAD
 DIRECTORYKEYMERKELROOT=$HEAD
@@ -612,10 +723,6 @@ until [ "$DIRECTORYKEYMERKELROOT" = "0000000000000000000000000000000000000000000
     done
 done
 echo
-
-: <<'EOF'
-
-EOF
 
 
 
---
+++ how-to-git.sh
@@ -0,0 +1,11 @@
+# how to git:
+# ----------
+# from ~/testing/testing/test-plans-and-scripts/
+git add .
+git commit -m "script updates"
+git push origin master
+
+#how to retrieve from git
+#------------------------
+#git pull
+
