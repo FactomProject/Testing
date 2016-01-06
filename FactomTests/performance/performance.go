a0dada8ce1819fd6bd9f2946a17892a3702201f0
Paul Snow <snow.paul@gmail.com>
2016-01-06 15:58:09 -0600

    Sync tests

--- FactomTests/performance/performance.go
+++ FactomTests/performance/performance.go
@@ -7,71 +7,19 @@ package main
 import (
 	"fmt"
 	"time"
+	"sync"
 )
 
-var (
-	test chan int
-	ret  chan int
-)
-
-func reflect() {
-	for {
-		v := <- test
-		ret <- v
-	}
-}
-
-type empty {}
-type semaphore chan empty
-var sem = make(semaphore, N)
-// acquire n resources
-func (s semaphore) P(n int) {
-	e := empty{}
-	for i := 0; i < n; i++ {
-		s <- e
-	}
-}
-
-// release n resources
-func (s semaphore) V(n int) { 
-	for i := 0; i < n; i++ {
-		<-s
-	}
-}
-
-func reflect2(i int) int {
-	fmt.Println("r2")
-	sem.P(1)
-	v := i
-	sem.V(1)
-	fmt.Println("v",v)
-	return v
-}
+var _ = fmt.Println
 
 func main() {
-	{
-	test = make(chan int)
-	ret = make(chan int)
 	
+	sem := new(sync.Mutex)
 	now := time.Now()
-	go reflect()
-	var sum int
 	var i int
-	for i=0; i<1000000; i++ {
-		test <- i
-		v := <- ret
-		sum += v
+	for i=0; i<10000000; i++ {
+		sem.Lock()
+		sem.Unlock()
 	}
-	fmt.Println(i, sum, time.Since(now))
-	}
-	{
-		lock = make(chan int)
-		now := time.Now()
-		var sum int
-		for i:=0; i<1000000; i++ {
-			v := reflect2(i)
-			sum += v
-		}
-		fmt.Println(sum, time.Since(now))
-	}
+	fmt.Println(i, time.Since(now))
 }
