3cbcb7f8ddea8969b015aac124c4f879e08a93a0
Justin Hanneman <hannemennah@gmail.com>
2016-03-10 13:42:33 -0600

    gofmt

--- examples/go/poloniex/poloniex_price.go
+++ examples/go/poloniex/poloniex_price.go
@@ -5,15 +5,15 @@
 package main
 
 import (
+	"bytes"
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
-	"log"
-    "net/http"
- 	"html/template"
 	"github.com/FactomProject/factom"
 	ed "github.com/agl/ed25519"
- 	"bytes"
+	"html/template"
+	"log"
+	"net/http"
 )
 
 var (
@@ -22,62 +22,62 @@ var (
 
 type OrderBook struct {
 	Timestamp int64
-	FCT_BTC json.RawMessage
-	BTC_USD json.RawMessage
+	FCT_BTC   json.RawMessage
+	BTC_USD   json.RawMessage
 }
 
 type PassJSON struct {
-    PoloniexData string
+	PoloniexData string
 }
 
 type ErrorChecker struct {
-    Error string
+	Error string
 }
 
 func main() {
-    //set up handler for generating graph
- 	http.HandleFunc("/", renderGraph)
-    //open user's browser, to view graph
-    Open("http://localhost:8094")
-    //start webserver     
-    http.ListenAndServe(":8094", nil)
-}  
-    
+	//set up handler for generating graph
+	http.HandleFunc("/", renderGraph)
+	//open user's browser, to view graph
+	Open("http://localhost:8094")
+	//start webserver
+	http.ListenAndServe(":8094", nil)
+}
+
 func renderGraph(w http.ResponseWriter, r *http.Request) {
 	es, err := getValidEntries()
 	if err != nil {
 		log.Fatal(err)
 	}
-    //used to store/pass JSON data
-    var buffer bytes.Buffer
-    //begin JSON array
-    buffer.WriteString("[")
+	//used to store/pass JSON data
+	var buffer bytes.Buffer
+	//begin JSON array
+	buffer.WriteString("[")
 	for i, e := range es {
-        //add Valid Entry to JSON array
-        buffer.WriteString(string(e.Content))
-		if i < len(es) - 1 {
-            //comma-delimit objects in JSON array
-		    buffer.WriteString(",")
+		//add Valid Entry to JSON array
+		buffer.WriteString(string(e.Content))
+		if i < len(es)-1 {
+			//comma-delimit objects in JSON array
+			buffer.WriteString(",")
 		}
 	}
-    //end JSON array
+	//end JSON array
 	buffer.WriteString("]")
-	
-    //parse d3.js template file
-    t, err := template.ParseFiles("linechart.html")
-    if err != nil {
-        fmt.Println("Template parse error: ", err.Error())
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-        return
-    }
-    
-    //prepare JSON array to be passed to page
-    passData := PassJSON{string(buffer.Bytes())}
 
-    //pass JSON array to be graphed with d3.js
-    if err := t.Execute(w, passData); err != nil {
-        http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
+	//parse d3.js template file
+	t, err := template.ParseFiles("linechart.html")
+	if err != nil {
+		fmt.Println("Template parse error: ", err.Error())
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	//prepare JSON array to be passed to page
+	passData := PassJSON{string(buffer.Bytes())}
+
+	//pass JSON array to be graphed with d3.js
+	if err := t.Execute(w, passData); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
 
 func getValidEntries() ([]*factom.Entry, error) {
@@ -85,14 +85,14 @@ func getValidEntries() ([]*factom.Entry, error) {
 	if err != nil {
 		return nil, err
 	}
-	
+
 	validEntries := make([]*factom.Entry, 0)
 	for _, e := range es {
 		if poloniexIsValid(e) {
 			validEntries = append(validEntries, e)
 		}
 	}
-	
+
 	return validEntries, nil
 }
 
@@ -101,14 +101,14 @@ func poloniexIsValid(e *factom.Entry) bool {
 	if p, err := hex.DecodeString("ca81e518e9a5519b7b218b85b13d73447f65c48c9c6f1b67db55a54ab48fc1de"); err == nil {
 		copy(pub[:], p)
 	}
-	
+
 	sig := new([64]byte)
 	copy(sig[:], e.ExtIDs[0])
-	
+
 	if !ed.Verify(pub, e.Content, sig) {
 		return false
 	}
-	
+
 	b := new(OrderBook)
 	if err := json.Unmarshal(e.Content, b); err != nil {
 		return false
@@ -116,19 +116,19 @@ func poloniexIsValid(e *factom.Entry) bool {
 		return false
 	}
 
-    var errCheck ErrorChecker
+	var errCheck ErrorChecker
 
 	if err := json.Unmarshal(b.FCT_BTC, &errCheck); err == nil {
-        if len(errCheck.Error) > 0 {
-            return false
-        }
+		if len(errCheck.Error) > 0 {
+			return false
+		}
 	}
 
 	if err := json.Unmarshal(b.BTC_USD, &errCheck); err == nil {
-        if len(errCheck.Error) > 0 {
-            return false
-        }
+		if len(errCheck.Error) > 0 {
+			return false
+		}
 	}
-	
+
 	return true
 }
