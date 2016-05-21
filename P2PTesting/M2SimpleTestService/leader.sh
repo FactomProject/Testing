32e943620eeedb9b73559685b16c83503f6c20aa
Nirvana <nirvana@myster.io>
2016-05-21 05:34:35 -0500

    Make leader more quiet

--- P2PTesting/M2SimpleTestService/leader.sh
+++ P2PTesting/M2SimpleTestService/leader.sh
@@ -3,4 +3,4 @@
 ./new_run_header.sh
 nohup ./factomd -count=5 -p2pPort="8108" \
 -peers="10.5.0.4:8108 10.5.0.5:8108 10.5.0.6:8108 10.5.0.7:8108 10.5.0.8:8108 10.5.0.9:8108 10.5.0.10:8108 10.5.0.11:8108" \
--netdebug=3 >>runlog.txt 2>&1 &
+-netdebug=1 >>runlog.txt 2>&1 &
