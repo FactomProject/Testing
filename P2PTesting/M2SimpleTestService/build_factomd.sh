ce1e9ef21c07dd4bdd7aa8b9cc73c641f516f348
Nirvana <nirvana@myster.io>
2016-05-21 02:27:40 -0500

    Update for alpha p2p network release.

--- P2PTesting/M2SimpleTestService/build_factomd.sh
+++ P2PTesting/M2SimpleTestService/build_factomd.sh
@@ -2,7 +2,6 @@
 
 TMPDIR="/tmp/factomd-p2p-test-build"
 mkdir $TMPDIR
-cd ../../
-#pwd
+cd "$GOPATH/src/github.com/FactomProject/factomd"
 echo "Building linux factomd and putting it in $TMPDIR"
 CGO_ENABLED=0 GOOS=linux go build -installsuffix cgo -o "$TMPDIR/factomd"
