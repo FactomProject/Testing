1b0ca69abe80b4ad7ecf001434f3a58b0c96f12a
Brian Deery <brian@factom.org>
2016-06-08 21:32:59 -0500

    modified to run on linux too

--- P2PTesting/M2SimpleTestService/all_config.sh
+++ P2PTesting/M2SimpleTestService/all_config.sh
@@ -4,10 +4,10 @@ echo "Configuring all of the machines"
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
   echo "Configuring: $line"
-  config_remote_test_box.sh $line
+  ./config_remote_test_box.sh $line
 done < "./leaders.conf"
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
   echo "Configuring: $line"
-  config_remote_test_box.sh $line
+  ./config_remote_test_box.sh $line
 done < "./followers.conf"
--- P2PTesting/M2SimpleTestService/all_update.sh
+++ P2PTesting/M2SimpleTestService/all_update.sh
@@ -8,11 +8,11 @@ if [ $? -eq 0 ]; then
 
     while IFS='' read -r line || [[ -n "$line" ]]; do
       echo "Updating: $line"
-      copy_files_to_test_box.sh $line
+      ./copy_files_to_test_box.sh $line
     done < "./leaders.conf"
 
     while IFS='' read -r line || [[ -n "$line" ]]; do
       echo "Updating: $line"
-      copy_files_to_test_box.sh $line
+      ./copy_files_to_test_box.sh $line
     done < "./followers.conf"
 fi
