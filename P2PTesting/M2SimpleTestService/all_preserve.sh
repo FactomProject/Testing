f7c322c7731cac145c71e43a7acbdc5312579168
Nirvana <nirvana@myster.io>
2016-07-01 19:22:27 -0500

    Update factomd.conf, implment all_preserve

--- P2PTesting/M2SimpleTestService/all_preserve.sh
+++ P2PTesting/M2SimpleTestService/all_preserve.sh
@@ -1,35 +1,60 @@
 #!/bin/bash +x
 
-# preserve does the heavy lifting
-function preserve(){
-# $1 is the passed in directory named
-# $2 is the ssh alias for the host
-local name="$1-$2"
-echo "Tar runlog and .factom to a file named $name.tz"
-ssh -n $2 "tar czf ~/$name.tz  ~/runlog.txt ~/.factom"
-}
-function remove(){
-# $1 is the passed in directory named
-# $2 is the ssh alias for the host
-local name="$1-$2"
-echo "removing file named $name.tz"
-ssh -n $2 "rm ~/$name.tz"
-}
+# # preserve does the heavy lifting
+# function preserve(){
+# # $1 is the passed in directory named
+# # $2 is the ssh alias for the host
+# local name="$1-$2"
+# echo "Tar runlog and .factom to a file named $name.tz"
+# ssh -n $2 "tar czf ~/$name.tz  ~/runlog.txt ~/.factom"
+# }
+# function remove(){
+# # $1 is the passed in directory named
+# # $2 is the ssh alias for the host
+# local name="$1-$2"
+# echo "removing file named $name.tz"
+# ssh -n $2 "rm ~/$name.tz"
+# }
 
-echo "Usage: all_preserve.sh <Name-of-Directory>"
+echo "Usage: all_preserve.sh"
 
-echo "Preserving runlog and .factom in a directory named $1"
+directory=`date +logs-%Y-%m-%d-%H_%M`
+path="./runlogs/"
 
+echo "Preserving runlog.txt and message_log.csv in a directory named $directory"
+
+mkdir $path$directory
 while IFS='' read -r line || [[ -n "$line" ]]; do
   echo "Preserving: $line"
-  preserve $1 $line 
+ssh -n $line "tar czf $line-runlog.tz runlog.txt"
+ssh -n $line "tar czf $line-messagelog.tz message_log.csv"
+scp $line:~/$line-runlog.tz $path$directory
+scp $line:~/$line-messagelog.tz $path$directory
+ssh -n $line "rm ~/$line-runlog.tz" 
+ssh -n $line "rm ~/$line-messagelog.tz"  
+cd $path$directory
+tar -xvf $line-runlog.tz -O > $line-runlog.txt
+tar -xvf $line-messagelog.tz -O > $line-messagelog.csv
+cd ../..
+  # preserve $1 $line 
   #  remove $1 $line 
 done < "./leaders.conf"
 
 while IFS='' read -r line || [[ -n "$line" ]]; do
   echo "Preserving: $line"
-  preserve $1 $line
+  # preserve $1 $line
   #  remove $1 $line 
+  ssh -n $line "tar czf $line-runlog.tz runlog.txt"
+ssh -n $line "tar czf $line-messagelog.tz message_log.csv"
+scp $line:~/$line-runlog.tz $path$directory
+scp $line:~/$line-messagelog.tz $path$directory
+ssh -n $line "rm ~/$line-runlog.tz"
+ssh -n $line "rm ~/$line-messagelog.tz"
+cd $path$directory
+tar -xvf $line-runlog.tz -O > $line-runlog.txt
+tar -xvf $line-messagelog.tz -O > $line-messagelog.csv
+rm *.tz
+cd ../..
 done < "./followers.conf"
 
 
--- P2PTesting/M2SimpleTestService/config_remote_test_box.sh
+++ P2PTesting/M2SimpleTestService/config_remote_test_box.sh
@@ -4,8 +4,9 @@ echo "Stopping factomd on $1 before updating."
 ssh -n $1 './stop.sh'
 echo "Copies the local authorized_keys to ~/.ssh/authorized_keys"
 scp ../../../FactomInc/ops/authorized_keys $1:~/.ssh/authorized_keys
-echo "Removing previous log file."
+echo "Removing previous log files."
 ssh -n $1 'echo \"Config Remote Test Box Reset\" > runlog.txt'
+ssh -n $1 "rm ~/message_log.csv"
 echo "Making the .factom directory, and m2 directory and copying factomd.conf"
 ssh -n $1 'rm -rf .factom; mkdir .factom; cd .factom; mkdir m2'
 scp factomd.conf $1:~/.factom/m2/
