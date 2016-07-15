620113fe40dcf35530668a4056e4db81c9d80711
Nirvana <nirvana@myster.io>
2016-07-15 03:21:15 -0500

    Back to normal database (was map) since this will be backbone of test network.

--- P2PTesting/M2SimpleTestService/leader.sh
+++ P2PTesting/M2SimpleTestService/leader.sh
@@ -1,4 +1,4 @@
 #!/bin/bash +x
 
 ./new_run_header.sh
-nohup ./factomd -db=Map -count=2 -NETWORK="TEST" -peers="10.5.0.4:8109 10.5.0.5:8109 10.5.0.6:8109 10.5.0.7:8109 10.5.0.8:8109 10.5.0.9:8109 10.5.0.10:8109 10.5.0.11:8109" -netdebug=1 >>runlog.txt 2>&1 &
+nohup ./factomd -network="TEST" -peers="10.5.0.4:8109 10.5.0.5:8109 10.5.0.6:8109 10.5.0.7:8109 10.5.0.8:8109 10.5.0.9:8109 10.5.0.10:8109 10.5.0.11:8109" -netdebug=1 >>runlog.txt 2>&1 &
