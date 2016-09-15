13504bdc1ab68363869b72b3cd4652d431fb01e0
VeenaGondkar <veena@factom.com>
2016-09-15 17:09:22 -0500

    added commands for follower2

--- P2PTesting/sync_test/start_vagrant_test.sh
+++ P2PTesting/sync_test/start_vagrant_test.sh
@@ -29,26 +29,34 @@ if [ $? -eq 0 ]; then
   ssh leader "date"
   echo "Follower Timestamp:"
   ssh follower "date"
-
-  echo "About to delete .factom"
-  ssh -n leader "pkill factomdl; pkill factom-walletd; pkill factom-cli"
-  ssh -n follower "pkill factomdl; pkill factom-walletd; pkill factom-cli"
-
+  echo "Follower2 Timestamp:"
+  ssh follower2 "date"
+  
+  echo "About to kill processes"
+  ssh -n leader "pkill factomd; pkill factom-walletd; pkill factom-cli"
+  ssh -n follower "pkill factomd; pkill factom-walletd; pkill factom-cli"
+  ssh -n follower2 "pkill factomd; pkill factom-walletd; pkill factom-cli"
+  
   echo "About to delete .factom"
   ssh -n leader "rm -rf ~/.factom"
   ssh -n follower "rm -rf ~/.factom"
+  ssh -n follower2 "rm -rf ~/.factom"
 
   echo "About to create .factom"
   ssh -n leader "mkdir ~/.factom"
   ssh -n follower "mkdir ~/.factom"
-
+  ssh -n follower2 "mkdir ~/.factom"
+  
   echo "About to create .factom/m2"
   ssh -n leader "mkdir ~/.factom/m2"
   ssh -n follower "mkdir ~/.factom/m2"
+  ssh -n follower2 "mkdir ~/.factom/m2"
+
 
   echo "Copying factomd.conf to ~/.factom/m2"
   ssh -n leader "cp /vagrant/files/factomd.conf ~/.factom/m2"
   ssh -n follower "cp /vagrant/files/factomd.conf ~/.factom/m2"
+  ssh -n follower2 "cp /vagrant/files/factomd.conf ~/.factom/m2"
 
   echo "Start the leader"
   ssh -n leader "cd /vagrant/files/ && ./leader.sh" 
@@ -70,7 +78,7 @@ if [ $? -eq 0 ]; then
   echo "Turn on latency on the follower"
   # Hit both network interfaces, because the NAT and private/host only seem to change between create/destroy.
   ssh -n follower "sudo tc qdisc add dev eth1 root netem delay 400ms"
-  ssh -n follower "sudo tc qdisc add dev eth0 root netem delay 400ms"
+  ssh -n follower2 "sudo tc qdisc add dev eth0 root netem delay 400ms"
 
   echo "Follower to leader Ping:"
   ssh -n follower "ping -c 3 10.0.99.3"
@@ -80,6 +88,7 @@ if [ $? -eq 0 ]; then
 
   echo "Start the follower"
   ssh -n follower "cd /vagrant/files/ && ./follower.sh"
+  ssh -n follower2 "cd /vagrant/files/ && ./follower2.sh"
 
   echo "Start the wallet"
   ssh -n follower "cd /vagrant/files/ && ./wallet.sh" 
@@ -93,6 +102,7 @@ if [ $? -eq 0 ]; then
   ssh -n leader "/vagrant/files/factom-cli listaddresses"
   ssh -n leader "/vagrant/files/factom-cli get allentries b69469af5a875cfd50786827e92171a84232bd7a198fa29234ac931e40a342c3"
   ssh -n follower "/vagrant/files/factom-cli get allentries b69469af5a875cfd50786827e92171a84232bd7a198fa29234ac931e40a342c3"
+  ssh -n follower2 "/vagrant/files/factom-cli get allentries b69469af5a875cfd50786827e92171a84232bd7a198fa29234ac931e40a342c3"
 
 
   echo "Block Heights. CTRL-C to quit."
@@ -100,7 +110,8 @@ if [ $? -eq 0 ]; then
     date
     L="$(ssh -n leader "/vagrant/files/factom-cli get height")"
     F="$(ssh -n follower "/vagrant/files/factom-cli get height")"
-    echo "Leader: ${L} Follower: ${F}"
+    F2="$(ssh -n follower2 "/vagrant/files/factom-cli get height")"
+    echo "Leader: ${L} Follower: ${F} Follower2: ${F2}"
     sleep 2
   done
 fi
