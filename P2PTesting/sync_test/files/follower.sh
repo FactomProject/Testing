9b07b0a5fd6571281de86aa653ceecda1c92f0bb
VeenaGondkar <veena@factom.com>
2016-09-15 17:10:02 -0500

    reduced block time to 6 seconds

--- P2PTesting/sync_test/files/follower.sh
+++ P2PTesting/sync_test/files/follower.sh
@@ -1,4 +1,4 @@
 #!/bin/bash +x
 
 
-nohup ./factomd -prefix="follower-" -peers="10.0.99.3:8110" -networkPort=8110 -network="LOCAL" -blktime=60 -netdebug=1 -exclusive=true > /vagrant/output/follower.out 2>&1 &
+nohup ./factomd -prefix="follower-" -peers="10.0.99.3:8110" -networkPort=8110 -network="LOCAL" -blktime=6 -netdebug=1 -exclusive=true > /vagrant/output/follower.out 2>&1 &
