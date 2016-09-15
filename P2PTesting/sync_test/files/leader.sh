ca2c98fee6ff139e199b405d0442ac0847aa18e2
VeenaGondkar <veena@factom.com>
2016-09-15 17:10:31 -0500

    changed blktime to 6

--- P2PTesting/sync_test/files/leader.sh
+++ P2PTesting/sync_test/files/leader.sh
@@ -1,3 +1,3 @@
 #!/bin/bash +x
 
-nohup ./factomd -peers="10.0.99.2:8110" -networkPort=8110 -network="LOCAL" -blktime=60 -netdebug=1 -exclusive=true > /vagrant/output/leader.out 2>&1 &
+nohup ./factomd -peers="10.0.99.2:8110" -networkPort=8110 -network="LOCAL" -blktime=6 -netdebug=1 -exclusive=true > /vagrant/output/leader.out 2>&1 &
