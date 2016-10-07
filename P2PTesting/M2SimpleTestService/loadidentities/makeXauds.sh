1dbff09cad1b0f21874ea1fb4ae0781a174e5367
Brian Deery <brian@factom.org>
2016-10-07 18:44:30 -0500

    allow scripts to work with linux

--- P2PTesting/M2SimpleTestService/loadidentities/makeXauds.sh
+++ P2PTesting/M2SimpleTestService/loadidentities/makeXauds.sh
@@ -3,7 +3,7 @@ COUNTER=$1
 HOST=$3
 echo "Promoting Audit Servers..."
 while [  $COUNTER -lt $2  ]; do
-	sh send.sh $COUNTER a a $HOST
+	bash send.sh $COUNTER a a $HOST
 	let COUNTER=COUNTER+1 
 	sleep 1s
 done
